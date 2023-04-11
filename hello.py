a07f105be9edb2e14b5410850a680098b321f65a
Ben <acc2ben@outlook.com>
2023-04-12 02:09:46 +0800

    extend comment

--- hello.py
+++ hello.py
@@ -3,9 +3,11 @@
 #author : zhangben
 """
     this is a comment stock
+    i love mythical animals.
 """
 import os
 print("hello world")
 print("this is amazing.")
+
 if __name__ == "__main__":
     print("em...")
